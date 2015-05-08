03d87b0715db45c3eecd3031805349552fb49cc6
Karen Rustad <karen@aerofs.com>
2015-05-08 12:54:53 -0700

    it sends email with url params in the email body

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,94 @@
 extern crate iron;
+extern crate urlencoded;
 extern crate router;
 extern crate smtp;
 #[macro_use]
 extern crate log;
-extern crate env_logger;
 
 use std::io::Read;
 use std::net::TcpStream;
+use std::collections::HashMap;
 use iron::prelude::*;
 use iron::status;
+use urlencoded::UrlEncodedQuery;
+use urlencoded::UrlEncodedBody;
+use urlencoded::QueryMap;
 use router::Router;
 use smtp::sender::{Sender, SenderBuilder};
 use smtp::mailer::EmailBuilder;
 
 fn main() {
-    env_logger::init().unwrap();
     info!("Starting up!");
     println!("Hello");
     let mut router = Router::new();
 
+    // ?first_name=Karen&last_name=Rustad&email=karen%40aerofs.com&phone=952-210-6598&org_name=&org_size=&title=&comment=Testing
+
     router.get("/", hello_world);
-    router.post("/set", post_email);
+    router.post("/sales", post_email);
 
     fn hello_world(_: &mut Request) -> IronResult<Response> {
         let payload = "Hi!!!!";
         Ok(Response::with((status::Ok, payload)))
     }
 
+    // fn log_params(req: &mut Request) -> IronResult<Response> {
+    //     // Extract the decoded data as hashmap, using the UrlEncodedQuery plugin.
+    //     match req.get_ref::<UrlEncodedQuery>() {
+    //         Ok(ref hashmap) => println!("Parsed GET request query string:\n {:?}", hashmap),
+    //         Err(ref e) => println!("{:?}", e)
+    //     };
+
+    //     Ok(Response::with((status::Ok, "Hello!")))
+    // }
+
+
+    // fn log_post_data(req: &mut Request) -> IronResult<Response> {
+    //     match req.get_ref::<UrlEncodedBody>() {
+    //         Ok(ref hashmap) => println!("Parsed POST request body:\n {:?}", hashmap),
+    //         Err(ref e) => println!("{:?}", e)
+    //     };
+
+    //     Ok(Response::with((status::Ok, "Hello!")))
+    // }
+
     // Receive a message by POST and play it back.
     fn post_email(request: &mut Request) -> IronResult<Response> {
-        // let mut payload = String::new();
-        // request.body.read_to_string(&mut payload).unwrap();
-        // println!("Hi: {}", payload);
+        let mut payload = String::new();
+        let mut email = "form".to_string();
+
+        match request.get_ref::<UrlEncodedQuery>() {
+            Ok(ref hashmap) => {
+                for (name, value) in hashmap.iter() {
+                    payload.push_str(name);
+                    payload.push_str(": ");
+                    payload.push_str(&value[0]);
+                    payload.push_str("\n");
+                }
+                if (hashmap.contains_key("email")) {
+                    email = hashmap["email"][0].clone();
+                }
+            },
+            Err(ref e) => println!("{:?}", e)
+        };
 
         let mut email_builder = EmailBuilder::new();
         email_builder = email_builder.to("karen@aerofs.com");
         let email = email_builder.from("inquiryform@aerofs.com")
-                        .body("Hello!")
-                        .subject("Bonjour!")
+                        .body(&payload)
+                        .subject(&format!("Inquiry from {}", email))
                         .build();
-        let sb = SenderBuilder::new(("127.0.0.1",25)).hello_name("localhost")
+        let sb = SenderBuilder::new(("devmail.aerofs.com",25)).hello_name("localhost")
                         .enable_connection_reuse(false);
 
-        println!("sb data: {:?}", sb.server_addr);
         let mut sender: Sender = sb.build();
-
-        //for _ in (1..5) {
-        //    let _ = sender.send(email.clone());
-        //}
         let result = sender.send(email);
         sender.close();
 
         match result {
             Ok(..) => println!("Email sent successfully"),
-            Err(error) => println!("What? {:?}", error),
+            Err(error) => println!("Error: {:?}", error),
         }
-        //result.unwrap();
 
         return Ok(Response::with((status::Ok, "")));
     }
